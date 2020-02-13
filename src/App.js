65b30ce0f4acba1d9af25d10e3f4c420d9b25e57
Louise Marissal <marissal.louise@gmail.com>
2020-02-13 18:29:52 +0100

    hide word ok now set onclick to show or not letters

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.scss";
-import Clavier from "./Clavier";
+import Clavier from "./components/Clavier";
+// import Devinette from "./components/Devinette";
 
 function App() {
+  const HIDE_LETTER = "_";
+
+  // get the ABC
   const letters = [
     "A",
     "B",
@@ -31,20 +35,54 @@ function App() {
     "Y",
     "Z"
   ];
-  const handleLetterClick = evt => {
-    evt.preventDefault();
-    console.log("CLICK", evt);
+  // Generate random words
+  let words = [
+    "ALLUMETTE",
+    "CIGARETTE",
+    "BIERE",
+    "TOMATE",
+    "POMMES",
+    "POIRE",
+    "VOITURE"
+  ];
+  // get random word from array
+  let guessWord = words[Math.floor(Math.random() * words.length)];
+
+  // Get each letter separately
+  const input = guessWord;
+  let output = [];
+  output = [...input.split("").join(" ")];
+
+  // Show or not letter if matched
+  let letterClicked = [];
+  const handleLetterClick = letter => {
+    letterClicked.push(letter);
+    // letterClickeds.includes(letter) ? letter === letterClicked : letter === "_";
   };
+  console.log(output);
 
   return (
     <div className="JeuContainer">
       <h1>Jeu du Pendu React</h1>
+      <div className="PenduDevinette">
+        {letterClicked.includes(letters)
+          ? output.map((w, index) => (
+              <span className="wordTitle" key={index}>
+                {w}
+              </span>
+            ))
+          : output.map((w, index) => (
+              <span className="wordTitle" key={index}>
+                _
+              </span>
+            ))}
+      </div>
       <div className="PenduClavier">
         {letters.map((letter, index) => (
           <Clavier
             key={index}
             letters={letter}
-            onClick={evt => handleLetterClick(evt)}
+            onClick={() => handleLetterClick(letter)}
           />
         ))}
       </div>
